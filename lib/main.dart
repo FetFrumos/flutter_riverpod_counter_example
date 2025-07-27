35f64e7c32d4beacf07bbb41ad5c7e5190a9c5a2
Andrii <ber911@gmail.com>
2025-07-27 21:35:20 +0300

    main - refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'pages/first_page.dart';
 
 void main() {
-  //runApp(const MyApp());
-
   runApp(ProviderScope(
     child: CounterApp(),
   ));
 }
 
 class CounterApp extends StatelessWidget {
-  const CounterApp({Key? key}) : super(key: key);
+  const CounterApp({super.key});
 
   @override
   Widget build(BuildContext context) {
